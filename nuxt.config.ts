a6e550bebcabfbfa2cce7e2a0b9ec4a1e9f90855
Martha Lucía Caicedo Morales <lucia.caicedo@pragma.com.co>
2025-08-18 23:27:42 -0500

    style: aplicar formato al archivo nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,6 @@ export default defineNuxtConfig({
   modules: ['@pinia/nuxt'],
   css: ['@/assets/styles/main.scss'],
   runtimeConfig: {
-    public: { apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:3001/api' }
-  }
-})
+    public: { apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:3001/api' },
+  },
+});

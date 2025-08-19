4cc788833df5079442246ad0d4f683f377df87a0
Martha Lucía Caicedo Morales <lucia.caicedo@pragma.com.co>
2025-08-19 01:03:30 -0500

    feat: crear dto para implementar lógica de guardar libro

---
+++ features/library/dto/save-book.dto.ts
@@ -0,0 +1,16 @@
+export interface SaveBookRequestDto {
+  key: string;                         
+  title: string;
+  author_name: string[];               
+  cover_i?: number;                    
+  first_publish_year?: number;         
+  openLibraryKeyNormalized: string;    
+  coverUrl?: string;                   
+  inMyLibrary?: boolean;               
+  review: string;                      
+  rating: number;                      
+}
+
+export interface SaveBookResponseDto {
+  id: string;                          
+}
